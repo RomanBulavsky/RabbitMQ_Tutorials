7f0a8bf995ec8085e0aabd252ca6cd75d9683ea9
Roman Bulavsky <roman.bulavsky@effective-soft.com>
2022-08-11 14:05:24 +0300

    first tutorial

---
+++ Receive/Receive.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            
+            channel.QueueDeclare(
+            //     The name of the queue. Pass an empty string to make the server generate a name.
+                                 queue: "hello",
+            //     Should this queue will survive a broker restart?
+                                 durable: false,
+            //     Should this queue use be limited to its declaring connection? Such a queue will
+            //     be deleted when its declaring connection closes.
+                                 exclusive: false,
+            //     Should this queue be auto-deleted when its last consumer (if any) unsubscribes?
+                                 autoDelete: false,
+            //     Optional; additional queue arguments, e.g. "x-queue-type"
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(
+            //     The name of the queue. Pass an empty string to make the server generate a name.
+                                 queue: "hello",
+            //     Should this queue will survive a broker restart?
+                                 durable: false,
+            //     Should this queue use be limited to its declaring connection? Such a queue will
+            //     be deleted when its declaring connection closes.
+                                 exclusive: false,
+            //     Should this queue be auto-deleted when its last consumer (if any) unsubscribes?
+                                 autoDelete: false,
+            //     Optional; additional queue arguments, e.g. "x-queue-type"
+                                 arguments: null);
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
