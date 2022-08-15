0d901067cb19a29688ae2e9fcea4f8f8d488d67f
Roman Bulavsky <roman.bulavsky@effective-soft.com>
2022-08-15 13:27:35 +0300

    refactored

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -9,8 +9,8 @@ class Worker
     public static async Task Main()
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
-        using(var connection = factory.CreateConnection())
-        using(var channel = connection.CreateModel())
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "task_queue",
                                  durable: true,
@@ -31,12 +31,9 @@ class Worker
 
                 int dots = message.Split('.').Length - 1;
                 Thread.Sleep(dots * 1000);
-                if(new Random().Next(10) > 5){
+                if (new Random().Next(10) > 5)
+                {
                     Console.WriteLine(" [x] Error");
-                    // this consumer tag identifies the subscription
-                    // when it has to be cancelled
-                    // channel.BasicCancel(channel.BasicConsume(queueName, false, consumer));
-                    // channel.BasicCancelNoWait(ea.ConsumerTag);
                     channel.BasicNack(deliveryTag: ea.DeliveryTag, multiple: false, requeue: true);
                     return;
                 }
@@ -51,12 +48,7 @@ class Worker
             channel.BasicConsume(queue: "task_queue",
                                  autoAck: false,
                                  consumer: consumer);
-            while(true){
-                Console.WriteLine(" [x] waiting.....");
-                await Task.Delay(1000);
-                Console.WriteLine(" Press [enter] to continue waiting.");
-                Console.ReadLine();
-            }
+            Console.ReadLine();
         }
     }
 }
