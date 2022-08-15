33c291fc6d50e1da52c892b28373194b934f046d
Roman Bulavsky <roman.bulavsky@effective-soft.com>
2022-08-15 19:52:28 +0300

    tutorial 3

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            while (true)
+            {
+                var message = System.Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes($"info: {message}");
+                channel.BasicPublish(exchange: "logs",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+}
